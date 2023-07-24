9f13b3f473d88996cda0238da2cbade9916f1922
lahiruhet <lahiru@tuta.io>
2023-07-24 21:40:05 +0530

    added create route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const blog = require('./models/blogs')
+const bodyParser = require('body-parser')
 const app = express()
-const dbconnect = 'mongodb+srv://root:Gim7mrgPCiFwMNnd@cluster0.3xdbpb5.mongodb.net/justanotherblog?retryWrites=true&w=majority'
+const dbconnect = 'mongodb+srv://lahiru:40870ananda@cluster0.3xdbpb5.mongodb.net/?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
 mongoose.connect(dbconnect,{useNewUrlParser:true,useUnifiedTopology:true})
 .then((result)=>console.log('connected to db'))
@@ -10,25 +12,32 @@ mongoose.connect(dbconnect,{useNewUrlParser:true,useUnifiedTopology:true})
 
 app.set('view engine','ejs')
 app.listen(3000)
-
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
-app.get('/create',(req,res)=>{
-    const newBlog = new blog({
-        title:'new blog 2',
-        body:'new blog body'
-    })
-    newBlog.save().then(result=>res.send(result)).catch(err=>console.log(err))
-})
 
+app.get('/blogs',(req,res)=>{
+    blog.find().then(result=>res.send(result)).catch(err=>console.log(err))
+})
 
 app.get('/about',(req,res)=>{
     res.render('about')
 })
+app.get('/form',(req,res)=>{
+    res.render('form')
+})
 
-app.get((req,res)=>{
-    res.render('404')}
+app.post('/form',(req,res)=>{
+    const {title, author, body} = req.body
+    const newBlog = new blog({title, author, body})
+    newBlog.save().then(result=>res.send(result)).catch(err=>console.log(err))
+})
+
+app.use((req,res)=>{
+    res.status(404).render('404')}
     )
--- models/blogs.js
+++ models/blogs.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
     title:{type:String,required:true},
+    author:{type:String,required:true},
     body:{type:String,required:true},
 },{timestamps:true})
 
